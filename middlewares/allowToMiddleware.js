5d994f5bfa02be08003f5ce1466224772d470fd6
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-04-05 21:14:09 +0200

    Add authorization files

---
+++ middlewares/allowToMiddleware.js
@@ -0,0 +1,12 @@
+const allowTo = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return next(
+        new AppError("You don't have permission to access this resource", 403),
+      );
+    }
+    next();
+  };
+};
+
+export default allowTo;
