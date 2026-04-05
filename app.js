5f0514a60058305fe4e1e0d897fba9295d4dabb7
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-04-05 21:14:08 +0200

    Add project setup files

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import notFound from "./utils/notFound.js";
+import errorHandler from "./utils/errorHandler.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+};
+
+startServer();
