4b64c79887a4fe96adee6fa48a24c1e06dee0da3
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-04-06 20:24:31 +0200

    fix(validation-error): standardize field validation error structure

--- middlewares/validateMiddleware.js
+++ middlewares/validateMiddleware.js
@@ -1,13 +1,17 @@
 import { validationResult } from "express-validator";
+import AppError from "../utils/appError.js";
 
 const validate = (validations) => async (req, res, next) => {
   await Promise.all(validations.map((v) => v.run(req)));
   const errors = validationResult(req);
   if (errors.isEmpty()) return next();
 
-  const err = new Error("Validation failed");
-  err.validationErrors = errors.array();
-  next(err);
+  const validationData = errors.array().reduce((acc, error) => {
+    acc[error.path] = error.msg;
+    return acc;
+  }, {});
+
+  next(new AppError("Validation failed", 400, validationData));
 };
 
 export default validate;
